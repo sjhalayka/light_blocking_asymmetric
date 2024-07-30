fced547f558274a7e4c05ef5946ac53aa73f9632
Shawn Halayka <sjhalayka@gmail.com>
2024-07-30 12:23:42 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,212 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "main.h"
+
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+#pragma comment(lib, "opencv_world4100.lib")
+
+
+
+
+int main(int argc, char** argv)
+{
+	int tile_size = 18;
+
+	int num_input_channels = 4;
+
+	// Load from file
+	Mat input_mat = imread("input.png", IMREAD_UNCHANGED);
+
+	if (input_mat.empty() || input_mat.channels() != 4)
+	{
+		cout << "input.png must be a 32-bit PNG" << endl;
+		return -1;
+	}
+
+	const int res_x = input_mat.cols;
+	const int res_y = input_mat.rows;
+
+	Mat input_mat_backup = input_mat.clone();
+
+	resize(input_mat, input_mat, cv::Size(res_x / tile_size, res_y / tile_size), 0, 0, cv::INTER_NEAREST);
+
+
+
+
+
+
+
+	Mat input_light_mat = imread("input_light.png", IMREAD_UNCHANGED);
+
+	if (input_light_mat.empty() || input_light_mat.channels() != 4)
+	{
+		cout << "input_light.png must be a 32-bit PNG" << endl;
+		return -1;
+	}
+
+	resize(input_light_mat, input_light_mat, cv::Size(res_x / tile_size, res_y / tile_size), 0, 0, cv::INTER_NEAREST);
+
+
+
+
+	Mat input_light_blocking_mat = imread("input_light_blocking.png", IMREAD_UNCHANGED);
+
+	if (input_light_blocking_mat.empty() || input_light_blocking_mat.channels() != 4)
+	{
+		cout << "input_light_blocking.png must be a 32-bit PNG" << endl;
+		return -1;
+	}
+
+	resize(input_light_blocking_mat, input_light_blocking_mat, cv::Size(res_x / tile_size, res_y / tile_size), 0, 0, cv::INTER_NEAREST);
+
+
+
+
+
+
+
+
+
+	int num_output_channels = 4;
+
+	vector<float> output_pixels(res_x / tile_size * res_y / tile_size * num_output_channels, 0.0f);
+	vector<float> input_pixels(res_x / tile_size * res_y / tile_size * num_input_channels, 0.0f);
+	vector<float> input_light_pixels(res_x / tile_size * res_y / tile_size * num_input_channels, 0.0f);
+	vector<float> input_light_blocking_pixels(res_x / tile_size * res_y / tile_size * num_input_channels, 0.0f);
+
+	for (size_t y = 0; y < res_y / tile_size; y++)
+	{
+		for (size_t x = 0; x < res_x / tile_size; x++)
+		{
+			size_t uc_index = 4 * (x * res_y / tile_size + y);
+
+			Vec4b pixelValue = input_mat.at<Vec4b>(x, y);
+
+			input_pixels[uc_index + 0] = pixelValue[0] / 255.0f;// input_mat.data[uc_index + 0] / 255.0f;
+			input_pixels[uc_index + 1] = pixelValue[1] / 255.0f;// input_mat.data[uc_index + 1] / 255.0f;
+			input_pixels[uc_index + 2] = pixelValue[2] / 255.0f;// input_mat.data[uc_index + 2] / 255.0f;
+			input_pixels[uc_index + 3] = 1.0;
+		}
+	}
+
+
+	for (size_t y = 0; y < res_y / tile_size; y++)
+	{
+		for (size_t x = 0; x < res_x / tile_size; x++)
+		{
+			size_t uc_index = 4 * (x * res_y / tile_size + y);
+
+			Vec4b pixelValue = input_light_mat.at<Vec4b>(x, y);
+
+			input_light_pixels[uc_index + 0] = pixelValue[0] / 255.0f;// input_mat.data[uc_index + 0] / 255.0f;
+			input_light_pixels[uc_index + 1] = pixelValue[1] / 255.0f;// input_mat.data[uc_index + 1] / 255.0f;
+			input_light_pixels[uc_index + 2] = pixelValue[2] / 255.0f;// input_mat.data[uc_index + 2] / 255.0f;
+			input_light_pixels[uc_index + 3] = 0.0;
+		}
+	}
+
+
+	for (size_t y = 0; y < res_y / tile_size; y++)
+	{
+		for (size_t x = 0; x < res_x / tile_size; x++)
+		{
+			size_t uc_index = 4 * (x * res_y / tile_size + y);
+
+			Vec4b pixelValue = input_light_blocking_mat.at<Vec4b>(x, y);
+
+			input_light_blocking_pixels[uc_index + 0] = pixelValue[0] / 255.0f;// input_mat.data[uc_index + 0] / 255.0f;
+			input_light_blocking_pixels[uc_index + 1] = pixelValue[1] / 255.0f;// input_mat.data[uc_index + 1] / 255.0f;
+			input_light_blocking_pixels[uc_index + 2] = pixelValue[2] / 255.0f;// input_mat.data[uc_index + 2] / 255.0f;
+			input_light_blocking_pixels[uc_index + 3] = 1.0;
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// Initialize OpenGL / compute shader / textures
+	GLuint compute_shader_program = 0;
+	GLuint tex_output = 0, tex_input = 0, tex_light_input = 0, tex_light_blocking_input = 0;
+
+
+
+
+	if (false == init_all(
+		argc, argv,
+		tex_output,
+		tex_input,
+		tex_light_input,
+		tex_light_blocking_input,
+		res_x / tile_size, res_y / tile_size,
+		compute_shader_program))
+	{
+		cout << "Aborting" << endl;
+		return -1;
+	}
+
+	cout << "Computing " << endl;
+
+
+	// Run the compute shader
+	compute(tex_output,
+		tex_input,
+		tex_light_input,
+		tex_light_blocking_input,
+		res_x / tile_size, res_y / tile_size,
+		compute_shader_program,
+		output_pixels,
+		input_pixels,
+		input_light_pixels,
+		input_light_blocking_pixels);
+
+	cout << "Computing done" << endl;
+
+
+	// Save output to PNG file
+	Mat uc_output(res_x / tile_size, res_y / tile_size, CV_8UC4);
+
+	for (size_t x = 0; x < 4 * ((res_x / tile_size) * (res_y / tile_size)); x += 4)
+	{
+		uc_output.data[x + 0] = static_cast<unsigned char>(output_pixels[x + 0] * 255.0);
+		uc_output.data[x + 1] = static_cast<unsigned char>(output_pixels[x + 1] * 255.0);
+		uc_output.data[x + 2] = static_cast<unsigned char>(output_pixels[x + 2] * 255.0);
+		uc_output.data[x + 3] = 255;
+	}
+
+	//transpose(uc_output, uc_output);
+
+	resize(uc_output, uc_output, cv::Size(res_x, res_y), 0, 0, cv::INTER_LINEAR);
+
+	for (size_t x = 0; x < (res_x * res_y * 4); x += 4)
+	{
+		uc_output.data[x + 0] = static_cast<unsigned char>(255.0 * (input_mat_backup.data[x + 0] / 255.0 * uc_output.data[x + 0] / 255.0));
+		uc_output.data[x + 1] = static_cast<unsigned char>(255.0 * (input_mat_backup.data[x + 1] / 255.0 * uc_output.data[x + 1] / 255.0));
+		uc_output.data[x + 2] = static_cast<unsigned char>(255.0 * (input_mat_backup.data[x + 2] / 255.0 * uc_output.data[x + 2] / 255.0));
+		uc_output.data[x + 3] = 255;
+	}
+
+	imwrite("out.png", uc_output);
+
+	//cout << "Save done" << endl;
+
+
+
+	// Clean up all memory
+	delete_all(tex_output, tex_input, tex_light_input, tex_light_blocking_input, compute_shader_program);
+
+
+
+	return 0;
+}
---
+++ main.h
@@ -0,0 +1,285 @@
+// Source code by Shawn Halayka
+// Source code is in the public domain
+
+
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <cmath>
+#include <cstdlib>
+using namespace std;
+
+#include <GL/glew.h>
+#include <GL/glut.h>
+
+// Automatically link in the GLUT and GLEW libraries if compiling on MSVC++
+#ifdef _MSC_VER
+#pragma comment(lib, "freeglut")
+#pragma comment(lib, "glew32")
+#endif
+
+void compute(GLuint& tex_output, 
+	GLuint& tex_input,
+	GLuint& tex_light_input,
+	GLuint& tex_light_blocking_input,
+	GLint tex_w, GLint tex_h, 
+	GLuint& compute_shader_program, 
+	vector<float> &output_pixels, 
+	const vector<float> &input_pixels,
+	const vector<float>& input_light_pixels,
+	const vector<float>& input_light_blocking_pixels)
+{
+	glActiveTexture(GL_TEXTURE1);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, tex_w, tex_h, 0, GL_RGBA, GL_FLOAT, &input_pixels[0]);
+	glBindImageTexture(1, tex_input, 0, GL_FALSE, 0, GL_READ_ONLY, GL_RGBA32F);
+
+	glActiveTexture(GL_TEXTURE2);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, tex_w, tex_h, 0, GL_RGBA, GL_FLOAT, &input_light_pixels[0]);
+	glBindImageTexture(2, tex_light_input, 0, GL_FALSE, 0, GL_READ_ONLY, GL_RGBA32F);
+
+	glActiveTexture(GL_TEXTURE3);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, tex_w, tex_h, 0, GL_RGBA, GL_FLOAT, &input_light_blocking_pixels[0]);
+	glBindImageTexture(3, tex_light_blocking_input, 0, GL_FALSE, 0, GL_READ_ONLY, GL_RGBA32F);
+
+	glUniform1i(glGetUniformLocation(compute_shader_program, "input_image"), 1); 
+	glUniform1i(glGetUniformLocation(compute_shader_program, "input_light_image"), 2);
+	glUniform1i(glGetUniformLocation(compute_shader_program, "input_light_blocking_image"), 3);
+	glUniform2i(glGetUniformLocation(compute_shader_program, "u_size"), tex_w, tex_h);
+
+	// Run compute shader
+	glDispatchCompute((GLuint)tex_w, (GLuint)tex_h, 1);
+
+	// Wait for compute shader to finish
+	glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+
+	// Copy output pixel array to CPU as texture 0
+	glActiveTexture(GL_TEXTURE0);
+	glBindImageTexture(0, tex_output, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
+	glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_FLOAT, &output_pixels[0]);
+}
+
+void init_textures(GLuint &tex_output, GLuint &tex_input, GLuint &tex_light_input, GLuint &tex_light_blocking_input, GLuint tex_w, GLuint tex_h)
+{
+	// Generate and allocate output texture
+	glGenTextures(1, &tex_output);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, tex_output);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, tex_w, tex_h, 0, GL_RGBA, GL_FLOAT, NULL);
+	glBindImageTexture(0, tex_output, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
+
+	// Generate input texture
+	glGenTextures(1, &tex_input);
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, tex_input);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+	glGenTextures(1, &tex_light_input);
+	glActiveTexture(GL_TEXTURE2);
+	glBindTexture(GL_TEXTURE_2D, tex_light_input);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+	glGenTextures(1, &tex_light_blocking_input);
+	glActiveTexture(GL_TEXTURE3);
+	glBindTexture(GL_TEXTURE_2D, tex_light_blocking_input);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+}
+
+
+
+bool compile_and_link_compute_shader(const char *const file_name, GLuint &program)
+{
+	// Read in compute shader contents
+	ifstream infile(file_name);
+
+	if (infile.fail())
+	{
+		cout << "Could not open " << file_name << endl;
+		return false;
+	}
+
+	string shader_code;
+	string line;
+
+	while (getline(infile, line))
+	{
+		shader_code += line;
+		shader_code += "\n";
+	}
+
+	// Compile compute shader
+	const char* cch = 0;
+	GLint status = GL_FALSE;
+
+	GLuint shader = glCreateShader(GL_COMPUTE_SHADER);
+	glShaderSource(shader, 1, &(cch = shader_code.c_str()), NULL);
+	glCompileShader(shader);
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+
+	if (GL_FALSE == status)
+	{
+		string status_string = "Compute shader compile error.\n";
+		vector<GLchar> buf(4096, '\0');
+		glGetShaderInfoLog(shader, 4095, 0, &buf[0]);
+
+		for (size_t i = 0; i < buf.size(); i++)
+			if ('\0' != buf[i])
+				status_string += buf[i];
+
+		status_string += '\n';
+
+		cout << status_string << endl;
+
+		glDeleteShader(shader);
+
+		return false;
+	}
+
+	// Link compute shader
+	program = glCreateProgram();
+	glAttachShader(program, shader);
+	glLinkProgram(program);
+	glGetProgramiv(program, GL_LINK_STATUS, &status);
+
+	if (GL_FALSE == status)
+	{
+		string status_string = "Program link error.\n";
+		vector<GLchar> buf(4096, '\0'); 
+		glGetShaderInfoLog(program, 4095, 0, &buf[0]);
+
+		for (size_t i = 0; i < buf.size(); i++)
+			if ('\0' != buf[i])
+				status_string += buf[i];
+
+		status_string += '\n';
+
+		cout << status_string << endl;
+
+		glDetachShader(program, shader);
+		glDeleteShader(shader);
+		glDeleteProgram(program);
+
+		return false;
+	}
+
+	// The shader is no longer needed now that the program
+	// has been linked
+	glDetachShader(program, shader);
+	glDeleteShader(shader);
+
+	return true;
+}
+
+bool init_opengl_4_3(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_RGBA);
+	int glut_window_handle = glutCreateWindow("");
+
+	if (!(GLEW_OK == glewInit() &&
+		GLEW_VERSION_4_3))
+	{
+		return false;
+	}
+
+	return true;
+}
+
+bool init_all(int argc, char** argv, 
+	GLuint& tex_output, 
+	GLuint& tex_input, 
+	GLuint& tex_light_input,
+	GLuint& tex_light_blocking_input,
+	GLint tex_w, GLint tex_h, 
+	GLuint& compute_shader_program)
+{
+	// Initialize OpenGL
+	if (false == init_opengl_4_3(argc, argv))
+	{
+		cout << "OpenGL 4.3 initialization failure" << endl;
+		return false;
+	}
+	else
+	{
+		cout << "OpenGL 4.3 initialization OK" << endl;
+	}
+
+	// Check for non-POT texture support
+	if (!GLEW_ARB_texture_non_power_of_two)
+	{
+		cout << "System does not support non-POT textures" << endl;
+		return false;
+	}
+	else
+	{
+		cout << "System supports non-POT textures" << endl;
+	}
+
+	// Initialize the compute shader
+	compute_shader_program = 0;
+
+	if (false == compile_and_link_compute_shader("shader.comp", compute_shader_program))
+	{
+		cout << "Failed to initialize compute shader" << endl;
+		return false;
+	}
+
+
+	cout << "Texture size: " << tex_w << "x" << tex_h << endl;
+
+	// Check that the global workgrounp count is greater than or equal to the input/output textures
+	GLint global_workgroup_count[2];
+	glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, 0, &global_workgroup_count[0]);
+	glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, 1, &global_workgroup_count[1]);
+
+	cout << "Max global workgroup size: " << global_workgroup_count[0] << "x" << global_workgroup_count[1] << endl;
+
+	if (tex_w > global_workgroup_count[0])
+	{
+		cout << "Texture width " << tex_w << " is larger than max " << global_workgroup_count[0] << endl;
+		return false;
+	}
+
+	if (tex_h > global_workgroup_count[1])
+	{
+		cout << "Texture height " << tex_h << " is larger than max " << global_workgroup_count[1] << endl;
+		return false;
+	}
+
+	init_textures(tex_output, tex_input, tex_light_input, tex_light_blocking_input, tex_w, tex_h);
+	
+	// Use the compute shader
+	glUseProgram(compute_shader_program);
+
+	return true;
+}
+
+void delete_all(GLuint& tex_output, GLuint& tex_input, GLuint& tex_light_input, GLuint& tex_light_blocking_input, GLuint& compute_shader_program)
+{
+	glDeleteTextures(1, &tex_output);
+	glDeleteTextures(1, &tex_input);
+	glDeleteTextures(1, &tex_light_input);
+	glDeleteTextures(1, &tex_light_blocking_input);
+
+	glDeleteProgram(compute_shader_program);
+}
+
+#endif
