39bb3abc15c34282665c2e3ba43b0c9c1c7fb82a
Shawn Halayka <sjhalayka@gmail.com>
2024-08-06 11:37:25 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -33,6 +33,138 @@ namespace std
 }
 
 
+
+
+/*
+
+// https://stackoverflow.com/questions/3696616/split-an-image-into-64x64-chunks
+#define CHUNKSIZE 36
+#define PIXEL_BYTES 4
+
+void ProcessChunk(Mat &m, int x, int y, int width, int height)
+{
+
+
+
+
+	for (int i = 0; i < width; i++)
+	{
+		for (int j = 0; j < height; j++)
+		{
+			Vec4b pixelValue = m.at<Vec4b>(j, i);
+
+			pixelValue[0] = 255;
+			pixelValue[0] = 0;
+			pixelValue[0] = 0;
+			pixelValue[0] = 255;
+
+			m.at<Vec4b>(j, i) = pixelValue;
+
+
+			//glm::vec3 colour(pixelValue[0] / 255.0f, pixelValue[1] / 255.0f, pixelValue[2] / 255.0f);
+
+
+
+			//unsigned char c0 = m.data[(j * width + i) * PIXEL_BYTES + 0];
+			//unsigned char c1 = m.data[(j * width + i) * PIXEL_BYTES + 1];
+			//unsigned char c2 = m.data[(j * width + i) * PIXEL_BYTES + 2];
+			//unsigned char c3 = m.data[(j * width + i) * PIXEL_BYTES + 3];
+
+			//c0 = 0;// rand() % 50;
+			//c1 = 0;//rand() % 50;
+			//c2 = 0;//rand() % 50;
+			//c3 = 0;//rand() % 50;
+
+			//m.data[(j * width + i) * PIXEL_BYTES + 0] = 255;
+			//m.data[(j * width + i) * PIXEL_BYTES + 1] = 0;
+			//m.data[(j * width + i) * PIXEL_BYTES + 2] = 0;
+			//m.data[(j * width + i) * PIXEL_BYTES + 3] = 255;
+
+
+		}
+	}
+}
+
+
+
+
+
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+
+void ProcessImage(Mat &m)
+{
+	int nWidth = m.cols;
+	int nHeight = m.rows;
+
+	for (int x = 0; x < nWidth; x += CHUNKSIZE)
+		for (int y = 0; y < nHeight; y += CHUNKSIZE)
+			ProcessChunk(m, x, y, MIN(nWidth - x, CHUNKSIZE), MIN(nHeight - y, CHUNKSIZE));
+}
+
+
+*/
+
+
+
+std::vector<cv::Mat> splitImage(cv::Mat& image, int M, int N)
+{
+	// All images should be the same size ...
+	int width = image.cols / M;
+	int height = image.rows / N;
+	// ... except for the Mth column and the Nth row
+	int width_last_column = width + (image.cols % width);
+	int height_last_row = height + (image.rows % height);
+
+	std::vector<cv::Mat> result;
+
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < M; ++j)
+		{
+			// Compute the region to crop from
+			cv::Rect roi(width * j,
+				height * i,
+				(j == (M - 1)) ? width_last_column : width,
+				(i == (N - 1)) ? height_last_row : height);
+
+			result.push_back(image(roi));
+		}
+	}
+
+	return result;
+}
+
+
+cv::Mat imageCollage(std::vector<cv::Mat>& array_of_images, int M, int N)
+{
+	// All images should be the same size
+	const cv::Size images_size = array_of_images[0].size();
+	// Create a black canvas
+	cv::Mat image_collage(images_size.height * N, images_size.width * M, CV_8UC4, cv::Scalar(0, 0, 0));
+
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < M; ++j)
+		{
+			if (((i * M) + j) >= array_of_images.size())
+				break;
+
+			cv::Rect roi(images_size.width * j, images_size.height * i, images_size.width, images_size.height);
+			array_of_images[(i * M) + j].copyTo(image_collage(roi));
+		}
+	}
+
+	return image_collage;
+}
+
+
+
+
+
+
+
+
 int main(int argc, char** argv)
 {
 	const int lighting_tile_size = 18;
@@ -48,13 +180,54 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
-
-
-
-
-	// Must be widescreen
 	const int res_x = input_mat.cols;
 	const int res_y = input_mat.rows;
+
+
+
+// https://stackoverflow.com/questions/64972297/how-to-stitch-an-array-of-images-into-an-mxn-image-collage/64973661#64973661
+// https://stackoverflow.com/questions/65091012/how-to-split-an-image-into-m-x-n-tiles
+
+
+	//std::vector<cv::Mat> array_of_images;
+	//array_of_images.push_back(cv::imread("1.jpg"));
+	//array_of_images.push_back(cv::imread("2.jpg"));
+	//cv::Mat image_collage = imageCollage(array_of_images, 3, 3);
+
+	//cv::imshow("Image Collage", image_collage);
+	//cv::waitKey(0);
+
+
+	int num_tiles_x = res_x / background_tile_size;
+	int num_tiles_y = res_y / background_tile_size;
+
+	std::vector<cv::Mat> array_of_images = splitImage(input_mat, num_tiles_x, num_tiles_y);
+
+	for (size_t a = 0; a < array_of_images.size(); a++)
+	{
+		for (int i = 0; i < array_of_images[a].cols; i++)
+		{
+			for (int j = 0; j < array_of_images[a].rows; j++)
+			{
+				Vec4b pixelValue = array_of_images[a].at<Vec4b>(j, i);
+
+				//pixelValue[0] = 0;
+				//pixelValue[1] = 0;
+				//pixelValue[2] = 255;
+				pixelValue[3] = 255;
+
+				array_of_images[a].at<Vec4b>(j, i) = pixelValue;
+			}
+		}
+	}
+
+	cout << array_of_images.size() << endl;
+
+	cv::Mat image_collage = imageCollage(array_of_images, num_tiles_x, num_tiles_y);
+
+	input_mat = image_collage.clone();
+
+
 
 	int largest_dim = res_x;
 
@@ -67,9 +240,15 @@ int main(int argc, char** argv)
 	input_mat.copyTo(square_mat(Rect(0, 0, res_x, res_y)));
 	input_mat = square_mat.clone();
 
+
 	Mat input_mat_backup = input_mat.clone();
 
+
 	resize(input_mat, input_mat, cv::Size(largest_dim / lighting_tile_size, largest_dim / lighting_tile_size), 0, 0, cv::INTER_NEAREST);
+
+
+
+
 
 	//	imwrite("input_mat.png", input_mat);
 
@@ -111,7 +290,7 @@ int main(int argc, char** argv)
 		}
 	}
 
-	cout << distinct_colour_map.size() << endl;
+//	cout << distinct_colour_map.size() << endl;
 
 	//for (vector<glm::vec3>::const_iterator ci = distinct_colours.begin(); ci != distinct_colours.end(); ci++)
 	//	cout << ci->x << " " << ci->y << " " << ci->z << endl;
